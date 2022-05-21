5c5c3ee753969c319d348d96f03bf85e828c40d6
SarunasJ-gif <sarunasj82@gmail.com>
2022-05-21 11:46:46 +0300

    print options buy tickets, show seats

--- src/com/sarunas/Main.java
+++ src/com/sarunas/Main.java
@@ -16,10 +16,27 @@ public class Main {
         System.out.println("Enter the number of seats in each row:");
         int columns = scanner.nextInt();
         System.out.println("> " + columns);
-
         String[][] seats = printCinemaSeats(rows, columns);
-        rezerveSeat(rows, seats);
-        printRezerveSeats(seats);
+
+
+        while(true) {
+            System.out.println("\n1. Show the seats");
+            System.out.println("2. Buy a ticket");
+            System.out.println("0. Exit");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    printRezerveSeats(seats);
+                    break;
+                case 2:
+                    buyTickets(rows, seats);
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Not valid option");
+            }
+        }
     }
 
     static int ticketPrice = 0;
@@ -27,26 +44,16 @@ public class Main {
 
     public static String[][] printCinemaSeats(int rows, int columns) {
         String[][] seats = new String[rows][columns];
-        System.out.println("Cinema:");
-        System.out.print(" ");
-        for (int i = 0; i < columns; i++) {
-            System.out.print(" " + (i + 1));
-        }
-        System.out.println();
-
         for (int i = 0; i < seats.length; i++) {
-            System.out.print(i + 1 + " ");
             for (int j = 0; j < seats[0].length; j++) {
-                seats[i][j] = "S";
-                System.out.print(seats[i][j] + " ");
+                seats[i][j] = "S";;
                 numberOfSeats++;
             }
-            System.out.println();
         }
         return seats;
     }
 
-    public static void rezerveSeat(int rows, String[][] seats) {
+    public static void buyTickets(int rows, String[][] seats) {
         System.out.println("\nEnter a row number:");
         int rowNumber = scanner.nextInt() - 1;
         System.out.println("> " + (rowNumber + 1));
@@ -60,8 +67,6 @@ public class Main {
         if (numberOfSeats <= 60) {
             ticketPrice = 10;
             System.out.println("\nTicket price: $" + ticketPrice);
-
-
         } else {
             if (rowNumber < midleRows) {
                 ticketPrice = 10;
